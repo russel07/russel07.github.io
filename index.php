45738c80be8603e4aa960b74a03c26130fdbaa82
Russel Hussain <48565081+russel07@users.noreply.github.com>
2019-03-20 13:17:14 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+    <head>
+        <meta charset="UTF-8">
+        <meta content="IE=edge" http-equiv="X-UA-Compatible">
+        <meta name="viewport" content="width=device-width,initial-scale=1" >
+        <meta content="description" name="description">
+        <meta content="Portfolio templates have been developed by Md. Russel Hussain" name="author">
+        <title>Md. Russel Hussain</title>
+
+        <link rel="stylesheet" href="bulma/0.7.4/css/bulma.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/animate.min.css">
+
+        <script src="js/jquery.min.js"></script>
+        <script src="js/jquery1.11.4-ui.min.js"></script>
+        <script src="js/wow.min.js"></script>
+        <script> new WOW().init(); </script>
+
+    </head>
+
+    <body>
+        <section class="hero is-medium is-primary is-bold" style="margin-top: -5%;">
+            <div class="hero-body">
+                <div class="container has-text-centered">
+                    <h1 class="title animated wow fadeInLeft" data-wow-delay="0.5s" data-wow-duration="2s">Russel Hussain</h1>
+                    <p class="subtitle animated wow fadeInRight" data-wow-delay="0.75s" data-wow-duration="2s">Full Stack Web Developer</p>
+                    <span class="animated fadeInDown wow" data-wow-delay="0.5s">
+                        <span class="icon">
+                            <a class="icon fa-icon-2x" href="https://www.facebook.com/russel.hussain.7" title="Facebook">
+                                <i class="fa fa-facebook"></i>
+                            </a>
+                        </span>
+
+                        <span class="icon">
+                            <a class="icon fa-icon-2x" href="https://www.fiverr.com/mdrusselhussain" title="Fiverr">
+                                <i class="fa fa-twitter"></i>
+                            </a>
+                        </span>
+
+                        <span class="icon">
+                            <a class="icon fa-icon-2x" href="#" title="md.rhussain">
+                                <i class="fa fa-skype"></i>
+                            </a>
+                        </span>
+
+                        <span class="icon">
+                            <a class="icon fa-icon-2x" href="#" title="Google Plus">
+                                <i class="fa fa-google-plus"></i>
+                            </a>
+                        </span>
+
+                        <span class="icon">
+                            <a class="icon fa-icon-2x" href="#" title="md.russel.hussain@gmail.com">
+                                <i class="fa fa-envelope"></i>
+                            </a>
+                        </span>
+                    </span>
+                </div>
+
+                <div class="card animated bounceInUp wow" data-wow-delay=".5s"  style="margin-top: 2%;">
+                    <div class="card-header">
+                        <p class="card-header-title animated fadeInRight wow" data-wow-delay=".5s" data-wow-duration="2s">
+                            About Me
+                        </p>
+                    </div>
+                    <div class="card-content">
+                        <div class="columns">
+                            <div class="column is-one-quarter">
+                                <figure class="image is-1by1">
+                                    <img class="is-rounded" src="images/user.jpg">
+                                </figure>
+                            </div>
+                            <div class="column">
+                                <div class="content">
+                                    I have extensive experience developing web projects over the last seven years. I have worked on several web project using HTML, CSS, PHP and PHP Framework (Yii, Yii2, CodeIgniter, and Laravel), MySQL and JavaScript. I have the experience of managing developer team and lead the project as a team leader. I am very confident in my ability to work in a team. I would be grateful to work with an organization and serve well.
+                                    <hr>
+                                    I believe I have:
+                                    <ul>
+                                        <li>The ability to provide the best services to the clients to meet their needs.</li>
+                                        <li>Ability to work independently as well as a team.</li>
+                                        <li>Ability to work under pressure, dependable, highly-organized.</li>
+                                        <li>Ability to work with the provided deadline and budget.</li>
+                                        <li>Ability to provide the best works from me.</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card animated bounceInUp wow" data-wow-delay=".5s"  style="margin-top: 2%;">
+                    <div class="card-header">
+                        <p class="card-header-title animated fadeInRight wow" data-wow-delay=".5s" data-wow-duration="2s">
+                            My Services
+                        </p>
+                    </div>
+                    <div class="card-content">
+                        <div class="columns">
+                            <div class="column is-one-third">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <p class="card-header-title">
+                                            Web Development
+                                        </p>
+                                    </div>
+                                    <div class="card-content">
+                                        <div class="content">
+                                            I have extensive experience developing web projects over the last seven years. I have worked on several web project using HTML, CSS, PHP and PHP Framework (Yii, Yii2, CodeIgniter, and Laravel), MySQL and JavaScript. I have the experience of managing developer team and lead the project as a team leader. I am very confident in my ability to work in a team. I would be grateful to work with an organization and serve well.
+
+                                            I believe I have:
+                                            The ability to provide the best services to the clients to meet their needs.
+                                            Ability to work independently as well as a team.
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="column is-one-third">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <p class="card-header-title">
+                                            Web Application Development
+                                        </p>
+                                    </div>
+                                    <div class="card-content">
+                                        <div class="content">
+                                            I have extensive experience developing web projects over the last seven years. I have worked on several web project using HTML, CSS, PHP and PHP Framework (Yii, Yii2, CodeIgniter, and Laravel), MySQL and JavaScript. I have the experience of managing developer team and lead the project as a team leader. I am very confident in my ability to work in a team. I would be grateful to work with an organization and serve well.
+
+                                            I believe I have:
+                                            The ability to provide the best services to the clients to meet their needs.
+                                            Ability to work independently as well as a team.
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="column is-one-third">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <p class="card-header-title">
+                                            API
+                                        </p>
+                                    </div>
+                                    <div class="card-content">
+                                        <div class="content">
+                                            I have extensive experience developing web projects over the last seven years. I have worked on several web project using HTML, CSS, PHP and PHP Framework (Yii, Yii2, CodeIgniter, and Laravel), MySQL and JavaScript. I have the experience of managing developer team and lead the project as a team leader. I am very confident in my ability to work in a team. I would be grateful to work with an organization and serve well.
+
+                                            I believe I have:
+                                            The ability to provide the best services to the clients to meet their needs.
+                                            Ability to work independently as well as a team.
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card animated bounceInUp wow" data-wow-delay=".5s"  style="margin-top: 2%;">
+                    <div class="card-header">
+                        <p class="card-header-title animated fadeInRight wow" data-wow-delay=".5s" data-wow-duration="2s">
+                            Social Network
+                        </p>
+                    </div>
+
+                    <div class="card-content">
+                        <div class="columns">
+                            <div class="column">
+                                <div class="card animated rollIn wow" data-wow-delay=".5s">
+                                    <div class="card-content">
+                                                <span><span class="icon has-text-danger">
+                                                    <a class="icon fa-icon-2x" href="#" title="md.rhussain">
+                                                        <i class="fa fa-envelope"></i>
+                                                    </a>
+                                                </span>E-Mail</span>
+                                        <p>md.russel.hussain@gmail.com</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="column">
+                                <div class="card animated rollIn wow" data-wow-delay=".5s">
+                                    <div class="card-content">
+                                                <span><span class="icon has-text-info">
+                                                    <a class="icon fa-icon-2x" href="#" title="md.rhussain">
+                                                        <i class="fa fa-skype"></i>
+                                                    </a>
+                                                </span>Skype</span>
+                                        <p>
+                                            md.rhussain
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="column">
+                                <div class="card animated rollIn wow" data-wow-delay=".5s">
+                                    <div class="card-content">
+                                                <span><span class="icon has-text-info">
+                                                    <a class="icon fa-icon-2x" href="#" title="md.rhussain">
+                                                        <i class="fa fa-facebook"></i>
+                                                    </a>
+                                                </span>facebook</span>
+                                        <p>Md. Russel Hussain</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="column">
+                                <div class="card animated rollIn wow" data-wow-delay=".5s">
+                                    <div class="card-content">
+                                                <span><span class="icon">
+                                                    <a class="icon fa-icon-2x" href="#" title="md.rhussain">
+                                                        <i class="fa fa-address-card"></i>
+                                                    </a>
+                                                </span>fiverr</span>
+                                        <p>mdrusselhussain</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="hero-foot has-text-centered" style="margin-top: -5%;">
+                <p  class="subtitle animated wow fadeInRight" data-wow-delay="0.75s" data-wow-duration="2s">&copy; All right reserved </p>
+                <span class="animated wow bounceInLeft" data-wow-delay=".5s">
+                    <span class="icon">
+                        <a class="icon fa-icon-2x" href="https://www.facebook.com/russel.hussain.7" title="Facebook">
+                            <i class="fa fa-facebook"></i>
+                        </a>
+                    </span>
+
+                    <span class="icon">
+                        <a class="icon fa-icon-2x" href="https://www.fiverr.com/mdrusselhussain" title="Fiverr">
+                            <i class="fa fa-twitter"></i>
+                        </a>
+                    </span>
+
+                    <span class="icon">
+                        <a class="icon fa-icon-2x" href="#" title="md.rhussain">
+                            <i class="fa fa-skype"></i>
+                        </a>
+                    </span>
+
+                    <span class="icon">
+                        <a class="icon fa-icon-2x" href="#" title="Google Plus">
+                            <i class="fa fa-google-plus"></i>
+                        </a>
+                    </span>
+
+                    <span class="icon">
+                        <a class="icon fa-icon-2x" href="#" title="md.russel.hussain@gmail.com">
+                            <i class="fa fa-envelope"></i>
+                        </a>
+                    </span>
+                </span>
+            </div>
+        </section>
+    </body>
+</html>
